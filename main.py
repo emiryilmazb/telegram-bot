8e64c58790563c9c5d724a94f197b5783e96e26b
emrylmzb <emiryilmazb@gmail.com>
2023-08-03 17:37:42 +0300

    adding commands

--- main.py
+++ main.py
@@ -1,1 +1,75 @@
-print("hello world!")
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+with open("token.txt") as f:
+    token = f.read().strip()
+TOKEN: Final = token
+BOT_USERNAME: Final = "Bot_of_emir_bot"
+
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello there! General kenobi!")
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("there will be commands there")
+
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("this is a custom command")
+
+
+# responses
+
+def handle_response(text: str) -> str:
+    processed: str = text.lower()
+
+    if "hello" in processed:
+        return "Hello there! General kenobi!"
+    if "how are you" in processed:
+        return "I am good"
+
+    return "I don't understand you"
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f"user ({update.message.chat.id}) in {message_type}: '{text}'")
+
+    if message_type == "group":
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, "").strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+
+    print("bot:", response)
+    await update.message.reply_text(response)
+
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"Update {update} caused error {context.error}")
+
+
+if __name__ == "__main__":
+    print("starting bot...")
+    app = Application.builder().token(TOKEN).build()
+
+    # commands
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", start_command))
+    app.add_handler(CommandHandler("custom", start_command))
+
+    # messages
+    app.add_handler(MessageHandler(filters.text, handle_message))
+
+    # errors
+    app.add_error_handler(error)
+
+    print("polling...")
+    app.run_polling(poll_interval=3)
